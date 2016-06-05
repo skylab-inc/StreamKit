8474b4d5e466beb9f69b7611c190b33a35cd9664
Tyler Cloutier <cloutiertyler@aol.com>
2016-06-05 16:10:08 -0700

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "Reactive",
+    dependencies: [
+        .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 6)
+    ]
+)
