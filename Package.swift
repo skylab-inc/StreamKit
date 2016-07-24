46c8f031d50d5bc4d0cdcfc21aa3e48846f60062
Tyler Cloutier <cloutiertyler@aol.com>
2016-07-23 22:34:24 -0700

    Move to log v9

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Reflex",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 8)
+        .Package(url: "https://github.com/Zewo/Log.git", majorVersion: 0, minor: 9)
     ]
 )
